e75633a1839c8458de85346875db137295ea1d40
wjjang <ptabs232@gmail.com>
2023-08-11 17:00:19 +0900

    commit

---
+++ LFS/src/main/java/com/LFS/main/LfsApplication.java
@@ -0,0 +1,13 @@
+package com.LFS.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfsApplication.class, args);
+	}
+
+}
